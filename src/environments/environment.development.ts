69b02c84923d778ccbbc7f58f97997b84a1b38b5
Chrisi285 <Christianschlau888@googlemail.com>
2023-09-08 19:25:37 +0200

    Updated version number -> 0.1.5 Updated URL to https://api-hawk-softworks.de:5001

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   appVersion: require('../../package.json').version,
   production: true,
-  baseURL : "http://217.72.203.68:5000/api/v1"
+  baseURL : "https://api-hawk-softworks.de:5001/api/v1"
   // baseURL : "http://localhost:5000/api/v1"
 };
