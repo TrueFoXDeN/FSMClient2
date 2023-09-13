ea674798bfabd384303e900582b01fbdba96e690
Laurenz Dilba <laurenz.dilba@hotmail.de>
2023-09-13 17:46:35 +0200

    fixed opening multiple search callsing windows

--- src/app/services/searchcallsign.service.ts
+++ src/app/services/searchcallsign.service.ts
@@ -10,17 +10,24 @@ import {FlightstripService} from "../flightstrip-container/flightstrip.service";
 })
 export class SearchcallsignService {
 
+  isOpen: boolean = false
+
   constructor(private customStyle: CustomStyles, private fsService: FlightstripService,  public dialog: MatDialog) {
   }
 
   openSearchCallsign() {
-    const dialogConfig = new MatDialogConfig()
-    dialogConfig.height = `${150 * this.customStyle.multiplier}px`;
-    dialogConfig.width = `${300 * this.customStyle.multiplier}px`;
-    const dialogRef = this.dialog.open(SearchCallsignComponent, dialogConfig);
-    dialogRef.afterClosed().subscribe((data) => {
-      this.fsService.findFlightStrip(data);
-    });
+    if(!this.isOpen){
+      this.isOpen = true
+      const dialogConfig = new MatDialogConfig()
+      dialogConfig.height = `${150 * this.customStyle.multiplier}px`;
+      dialogConfig.width = `${300 * this.customStyle.multiplier}px`;
+      const dialogRef = this.dialog.open(SearchCallsignComponent, dialogConfig);
+      dialogRef.afterClosed().subscribe((data) => {
+        this.isOpen = false
+        this.fsService.findFlightStrip(data);
+      });
+    }
+
   }
 
 }
