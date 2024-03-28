5c307d0311a5eb56bebeaeb7854d41a002f0821e
Chrisi285 <Christianschlau888@googlemail.com>
2024-03-28 19:38:39 +0100

    Added create flightstrip in Multiplayer

--- src/app/services/commands/create-flightstrip-command.ts
+++ src/app/services/commands/create-flightstrip-command.ts
@@ -1,11 +1,22 @@
 import {Command} from "./command";
 import {Injectable} from "@angular/core";
+import {DataService} from "../data.service";
+import {FlightstripService} from "../../flightstrip-container/flightstrip.service";
+import {StripType} from "../../flightstrip-container/flightstrip.model";
+
 @Injectable({
   providedIn: 'root'
 })
 export class CreateFlightstripCommand implements Command {
+  constructor(private fsService: FlightstripService) {
+  }
+
   execute(args: string[]): void {
-    console.log('create flightstrip')
+
+    let colID = args[0];
+    let fsID = args[1];
+    let fsType: number = Number(args[2]);
+    this.fsService.createFlightstrip(colID, "", fsType, fsID);
   }
 
 }
