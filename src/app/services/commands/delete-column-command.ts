45e9c71381b5de8df7b92df4c3ec3fd5d4a4e706
Chrisi285 <Christianschlau888@googlemail.com>
2024-03-28 19:32:25 +0100

    Added delete column in Multiplayer

--- src/app/services/commands/delete-column-command.ts
+++ src/app/services/commands/delete-column-command.ts
@@ -1,11 +1,31 @@
 import {Command} from "./command";
 import {Injectable} from "@angular/core";
+import {DataService} from "../data.service";
+
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteColumnCommand implements Command {
+  constructor(private dataService: DataService) {
+  }
+
   execute(args: string[]): void {
-    console.log('delete column')
+
+    let colID = args[0];
+    let colIndex = -1;
+    for (let i = 0; i < this.dataService.profileData[this.dataService.currentProfileID].columnStructure.length; i++) {
+      if (this.dataService.profileData[this.dataService.currentProfileID].columnStructure[i].uuid === colID) {
+        colIndex = i;
+      }
+    }
+    if (colIndex > -1) {
+      this.dataService.profileData[this.dataService.currentProfileID].columnStructure.splice(colIndex, 1);
+
+    }
+
+    delete this.dataService.flightstripData[colID];
+
+
   }
 
 }
