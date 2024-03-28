c268cc44a6cf3583650a21d86e544b096c9b6c41
Chrisi285 <Christianschlau888@googlemail.com>
2024-03-28 20:31:24 +0100

    Added move flightstrip in Multiplayer

--- src/app/services/commands/move-flightstrip-command.ts
+++ src/app/services/commands/move-flightstrip-command.ts
@@ -1,11 +1,39 @@
 import {Command} from "./command";
 import {Injectable} from "@angular/core";
+import {FlightstripService} from "../../flightstrip-container/flightstrip.service";
+import {DataService} from "../data.service";
+import {Flightstrip} from "../../flightstrip-container/flightstrip.model";
+import {moveItemInArray, transferArrayItem} from "@angular/cdk/drag-drop";
+
 @Injectable({
   providedIn: 'root'
 })
 export class MoveFlightstripCommand implements Command {
+  constructor(private fsService: FlightstripService, private dataService: DataService) {
+  }
+
   execute(args: string[]): void {
-    console.log('get data')
+    let colId = args[0];
+    let fsID = args[1];
+    let newColId = args[2];
+    let newPos = Number(args[3]);
+    let fsIndex = this.fsService.getIndexInColumnByID(colId, fsID);
+    if (fsIndex === -1) {
+      console.log("Could not find flightstrip");
+      return;
+    }
+    if (colId === newColId) {
+      moveItemInArray(this.dataService.flightstripData[colId].flightstrips, fsIndex, newPos);
+    } else {
+      transferArrayItem(
+        this.dataService.flightstripData[colId].flightstrips,
+        this.dataService.flightstripData[newColId].flightstrips,
+        fsIndex,
+        newPos,
+      );
+    }
+    this.fsService.changedStripPos.next({id: fsID, newPosistion: newPos})
+
   }
 
 }
