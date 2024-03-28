ad9dc999edb08ab60b42d20179a38b4941cfe6ae
Chrisi285 <Christianschlau888@googlemail.com>
2024-03-28 19:51:13 +0100

    Added delete flightstrip in Multiplayer

--- src/app/services/commands/delete-flightstrip-command.ts
+++ src/app/services/commands/delete-flightstrip-command.ts
@@ -1,11 +1,27 @@
 import {Command} from "./command";
 import {Injectable} from "@angular/core";
+import {FlightstripService} from "../../flightstrip-container/flightstrip.service";
+import {DataService} from "../data.service";
+
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteFlightstripCommand implements Command {
+  constructor(private fsService: FlightstripService, private dataService: DataService) {
+  }
+
   execute(args: string[]): void {
-    console.log('delete flightstrip')
+    let colID = args[0];
+    let fsID = args[1];
+    let fsIndex = -1;
+    for (let i = 0; i < this.dataService.flightstripData[colID].flightstrips.length; i++) {
+      if (this.dataService.flightstripData[colID].flightstrips[i].id === fsID) {
+        fsIndex = i;
+      }
+    }
+    if (fsIndex > -1) {
+      this.dataService.flightstripData[colID].flightstrips.splice(fsIndex, 1);
+    }
   }
 
 }
