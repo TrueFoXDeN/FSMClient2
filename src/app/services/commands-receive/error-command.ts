057858d259712129ae17f7a6994bf773f5d897d5
Chrisi285 <Christianschlau888@googlemail.com>
2024-04-12 17:19:47 +0200

    EDIT [MP] Using get Data service instead of local code

--- src/app/services/commands-receive/error-command.ts
+++ src/app/services/commands-receive/error-command.ts
@@ -4,35 +4,19 @@ import {DataService} from "../data.service";
 import {ColumnBuilderService} from "../column-builder.service";
 import {MultiplayerSendService} from "../multiplayer-send.service";
 import {FlightstripService} from "../../flightstrip-container/flightstrip.service";
+import {SnackbarMessageService} from "../snackbar-message.service";
+import {MultiplayerGetDataService} from "../multiplayer-get-data.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ErrorCommand implements CommandReceive {
-  constructor(private mpSendService: MultiplayerSendService, private dataService: DataService, private columnBuilderService: ColumnBuilderService, private fsService: FlightstripService) {
+  constructor(private snackService: SnackbarMessageService, private multiplayerGetDataService: MultiplayerGetDataService) {
   }
 
   execute(args: any[]): void {
-    let data = args[1].data
-    let order = args[1].order
-    const columnIDs: string[] = Object.keys(data);
-    // let dashboard = [];
-    // for (let i = 0; i < columnIDs.length; i++) {
-    //   dashboard.push({x: i, y: 0, cols: 1, rows: 18, uuid: columnIDs[i], name: data[columnIDs[i]]["name"]});
-    // }
-    // this.dataService.profileData[this.dataService.currentProfileID].columnStructure = dashboard
-    this.columnBuilderService.columnConfigChanged.next()
-
-    for (let i = 0; i < columnIDs.length; i++) {
-      let columnID = columnIDs[i];
-      let fsIDs: string[] = order[columnID];
-      for (let k = 0; k < fsIDs.length; k++) {
-        let fsID = fsIDs[k];
-        let fsType: number = data[columnID][fsID]["type"]
-        this.fsService.createFlightstripWithObject(columnID, fsID, data[columnID][fsID], true)
-      }
-    }
-    console.log("RELOAD DATA!!!!!11!!!");
+    this.snackService.showMessage(`Error detected. Restored data from server`, "error");
+    this.multiplayerGetDataService.execute(args);
   }
 
 }
--- src/app/services/commands-receive/token-receive-command.ts
+++ src/app/services/commands-receive/token-receive-command.ts
@@ -20,29 +20,6 @@ export class TokenReceiveCommand implements CommandReceive {
     this.snackService.showMessage(`Connected to Multiplayer`, "success");
     this.cookieService.set('multiplayerAuth', args[0])
     this.multiplayerGetDataService.execute(args);
-    // this.dataService.currentProfileID = this.dataService.multiplayerProfileID
-    // this.dataService.profileData[this.dataService.multiplayerProfileID] = this.dataService.multiplayerProfile
-    // this.dataService.currentProfile = this.dataService.multiplayerProfile
-    // let data = args[1].data
-    // let order = args[1].order
-    // const columnIDs: string[] = Object.keys(data);
-    // let dashboard = [];
-    // for (let i = 0; i < columnIDs.length; i++) {
-    //   dashboard.push({x: i, y: 0, cols: 1, rows: 18, uuid: columnIDs[i], name: data[columnIDs[i]]["name"]});
-    // }
-    // this.dataService.profileData[this.dataService.currentProfileID].columnStructure = dashboard
-    // this.columnBuilderService.columnConfigChanged.next()
-    //
-    // for (let i = 0; i < columnIDs.length; i++) {
-    //   let columnID = columnIDs[i];
-    //   let fsIDs: string[] = order[columnID];
-    //   for (let k = 0; k < fsIDs.length; k++) {
-    //     let fsID = fsIDs[k];
-    //     let fsType: number = data[columnID][fsID]["type"]
-    //     //this.fsService.createFlightstrip(columnID, "", fsType, fsID);
-    //     this.fsService.createFlightstripWithObject(columnID, fsID, data[columnID][fsID], true)
-    //   }
-    // }
   }
 
 }
