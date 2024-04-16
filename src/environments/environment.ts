ff1dc2783be0b680dc17884895301aae1a7aaa84
Chrisi285 <Christianschlau888@googlemail.com>
2024-04-16 18:31:26 +0200

    Edit Changed WS Multiplayer port to 4001

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   appVersion: require('../../package.json').version,
   production: true,
   baseURL : "https://api-hawk-softworks.de:5001/api/v1",
-  websocketURL: "ws://api-hawk-softworks.de:4000",
+  websocketURL: "wss://api-hawk-softworks.de:4001",
   multiplayerURL: "https://api-hawk-softworks.de:5003/api/v1"
 };
