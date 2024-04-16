19c8880af761423018854d2829944b4c09f12ea4
Chrisi285 <Christianschlau888@googlemail.com>
2024-04-16 17:19:08 +0200

    Version bump 0.2.3 Edited Backend port to match ssl port on server

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  appVersion: require('../../package.json').version,
+  appVersion: require('../../package.json').version  + '-dev',
   production: false,
   baseURL : "https://api-hawk-softworks.de:5001/api/v1",
   websocketURL: "ws://192.168.10.14:4000",
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  appVersion: require('../../package.json').version + '-dev',
-  production: false,
+  appVersion: require('../../package.json').version,
+  production: true,
   baseURL : "https://api-hawk-softworks.de:5001/api/v1",
   websocketURL: "ws://api-hawk-softworks.de:4000",
-  multiplayerURL: "https://api-hawk-softworks.de:5002/api/v1"
+  multiplayerURL: "https://api-hawk-softworks.de:5003/api/v1"
 };
